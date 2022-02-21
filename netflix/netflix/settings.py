ce0a149eefeaea3bc52f6c40a60b5ebd91d130c4
Mens1s <ahmet_yigit_02@hotmail.com>
2022-02-21 19:55:29 +0300

    frontend

--- netflix/netflix/settings.py
+++ netflix/netflix/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from pickle import NONE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!7d5j-*^y103-rma$b1ta&7o=-u%cl$&cr5nbx3r*!c^$4n2fh'
+SECRET_KEY = NONE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
