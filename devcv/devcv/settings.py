d85ef139538a85a60b9a9de025c719fde6c52a1b
Mens1s <ahmet_yigit_02@hotmail.com>
2022-02-28 21:13:03 +0300

    'mistakes'

--- devcv/devcv/settings.py
+++ devcv/devcv/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "c$ex6o4@y*)yxn(_lslt-yiimkuh3l9io#ew2&x6j9ldi-r)ob"
+SECRET_KEY = None
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
